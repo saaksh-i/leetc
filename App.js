548f9f94584f918d77c32cf10433815ecd1c1aaf
saaksh-i <108679359+saaksh-i@users.noreply.github.com>
2025-04-21 22:23:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LeetCodeStats from './components/LeetCodeStats';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>LeetBoost Dashboard</h1>
+      <LeetCodeStats username="saakshii11" />
+    </div>
+  );
+}
+
+export default App;
